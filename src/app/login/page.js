7253c951228dafde0a452691e9ea26e1150d43d3
UmerAfzal <123937455+Umer237@users.noreply.github.com>
2023-10-23 19:23:44 +0500

    umer

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,7 +2,22 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='Login'>
+    <div className='Login-OneSide'>
+    <div className="Login-Flex">
+ 
+ <h2>Login Now</h2>
+
+<div className='textbox'>
+  
+<input type='email' placeholder='Enter your Email' />
+<input placeholder='Password' type='password'></input>
+
+<button><a href='/'>Login Now</a></button>
+ </div>
+ </div>
+ </div>
+</div>
   )
 }
 
