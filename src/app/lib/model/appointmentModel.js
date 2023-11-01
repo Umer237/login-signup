509729ad2db026aa7eea5e486ae8a2ebdbcda0a9
Your Name <burhanshabbir6189@gmail.com>
2023-11-01 18:10:47 +0500

    asa

--- src/app/lib/model/appointmentModel.js
+++ src/app/lib/model/appointmentModel.js
@@ -6,7 +6,7 @@ const AppModel = new mongoose.Schema({
     ContactNumber: String,
     Email: String,
     Procedure: String,
-    Date: String,
+    AppointDate: String,
     Time: String,
     Doctor: String,
     Message: String,
