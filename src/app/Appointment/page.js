8cf5f3b788748068f64da8d86581f07a4b0c661e
UmerAfzal <123937455+Umer237@users.noreply.github.com>
2023-11-01 14:50:45 +0500

    umer

--- src/app/Appointment/page.js
+++ src/app/Appointment/page.js
@@ -1,52 +1,40 @@
-'use client'
-import { AiOutlineArrowLeft } from 'react-icons/ai';
-import { AiOutlineArrowRight } from 'react-icons/ai';
-import React, { useState } from 'react'; 
-import Calendar from 'react-calendar'; 
-import 'react-calendar/dist/Calendar.css'; 
 
-const page = () => {
-  const [value, onChange] = useState(new Date()); 
+
+
+const Calendar = () => {
+
   return (
-    <>
-    <div className='calendar'>
-        <div className='header'>
-            <button id='prevBtn'>
-<AiOutlineArrowLeft/>
-            </button>
-            <div className='monthYear' id='monthYear'></div>
-            <button id='nextbtn'>
-                <AiOutlineArrowRight/>
-            </button>
-        </div>
-        <div className='days'>
-<div className='day'>Mon</div>
-<div className='day'>Tue</div>
-<div className='day'>Wed</div>
-<div className='day'>Thu</div>
-<div className='day'>Fri</div>
-<div className='day'>Sat</div>
-<div className='day'>Sun</div>
-        </div>
-        <div className='dates' id='dates'></div>
+    <>    <div className='Appointment-Inputs'>
+<h1>Dental Appointment</h1>
+<input type='text' placeholder='First Name'></input>
+<input type='text' placeholder='Last Name'></input>
+<input type='text' placeholder='Contact Number'></input>
+<input type='text' placeholder='Email'></input>
+<textarea></textarea>
+<select>
+  <option>Select Your Doctor</option>
+  <option>Doctor01</option>
+  <option>Doctor02</option>
+</select>
+<select>
+  <option>Time</option>
+  <option>1:00pm</option>
+  <option>1:30pm</option>
+  <option>2:00pm</option>
+  <option>2:30pm</option>
+  <option>3:00pm</option>
+  <option>3:30pm</option>
+  <option>4:00pm</option>
+  <option>4:30pm</option>
+  <option>5:00pm</option>
+  <option>5:30pm</option>
+  <option>6:00pm</option>
+</select>
     </div>
-    <div className='Appointment-BG'>
-      <h2>Dental Appointment</h2>
-    <div className='Appointment-Input'>
-        <input type='text' placeholder='Enter Your Name'></input>
-        <input type='text' placeholder='Date and Time'></input>
-        <input type='text' placeholder='Enter Phone Number'></input>
-        <textarea type='text' placeholder='Message'></textarea>
-    </div>
-    </div>
-    <div>
-    <Calendar 
-                onChange={onChange} 
-                value={value} 
-            /> 
-        </div> 
-    </>
-  )
-}
 
-export default page
+    </>
+
+  );
+};
+
+export default Calendar;
